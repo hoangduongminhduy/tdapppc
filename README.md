98928771c27505d9380e14edeafea23bfe1ca469
catapult04 <nhatminh232000@gmail.com>
2021-06-20 18:45:05 +0700

    ok

---
+++ README.md
@@ -0,0 +1,55 @@
+# TDVN
+
+<br />
+<p align="center">
+
+  <h3 align="center">TDVN - hỗ trợ tuyển dụng và tìm kiếm việc làm</h3>
+</p>
+
+[![React Native](https://img.shields.io/badge/ReactNative-0.60-blue)](https://facebook.github.io/react-native/)
+
+
+## Mục lục
+
+- [Giới_thiệu_đề_tài](#Giới_thiệu_đề_tài)
+- [Hướng_dẫn_cài_đặt](#Hướng_dẫn_cài_đặt)
+- [Tài_liệu_tham_khảo](#Tài_liệu_tham_khảo)
+
+
+## Giới_thiệu_đề_tài
+
+Sản phẩm được xây dựng với mục đích giúp cho việc kết nối tuyển dụng giữa nhà tuyển dụng và ứng viên được nhanh chóng và tiết kiệm chi phí
+
+## Hướng_dẫn_cài_đặt
+
+1. Clone repo
+
+```sh
+git clone https://github.com/catapult04/tdapppc.gỉt
+```
+
+2. Cài nodejs theo link:
+
+```sh
+https://nodejs.org/en/download/
+```
+
+3. Vào thư mục đã clone về và chạy lệnh
+
+```sh
+npm install
+```
+
+3. Bật developer mode trên điện thoại android, kết nối điện thoại với máy tính
+
+4. Chạy lệnh
+
+```sh
+npx react-native run-android
+```
+
+### Tài_liệu_tham_khảo
+
+- [React Native](https://facebook.github.io/react-native/docs/getting-started)
+- [React Native Navigator](https://reactnavigation.org/)
+
--- src/screen/Signup.js
+++ src/screen/Signup.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import {View, Text, StyleSheet, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import RNPickerSelect from 'react-native-picker-select';
+
 
 function checkSignup(){
-  
+
 }
 
 function Signup({navigation}) {
@@ -21,21 +22,10 @@ function Signup({navigation}) {
       <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
         <ScrollView>
 
-
-
-        <TextInput placeholder="Tên đăng nhập" style={styles.textInput}></TextInput>
-      <TextInput placeholder="Mật khẩu" secureTextEntry={true} style={styles.textInput}></TextInput>
-      <TextInput placeholder="Nhập lại mật khẩu" secureTextEntry={true} style={styles.textInput}></TextInput>
-      <TextInput placeholder="Họ tên" style={styles.textInput}></TextInput>
-      <View style={{flexDirection:'row'}}>
-        <TextInput placeholder="Năm sinh" keyboardType="numeric" style={styles.textInputRow}></TextInput>
-        <TextInput placeholder="Giới tính" style={styles.textInputRow}></TextInput>
-      </View>
-      <TextInput placeholder="Điện thoại" style={styles.textInput}></TextInput>
-      <TextInput placeholder="Địa chỉ" style={styles.textInput}></TextInput>
-
-
-
+          <TextInput placeholder="Tên đăng nhập" style={styles.textInput}></TextInput>
+          <TextInput placeholder="Mật khẩu" secureTextEntry={true} style={styles.textInput}></TextInput>
+          <TextInput placeholder="Nhập lại mật khẩu" secureTextEntry={true} style={styles.textInput}></TextInput>
+          <TextInput placeholder="Họ tên/ Tên doanh nghiệp" style={styles.textInput}></TextInput>
 
 
         </ScrollView>
@@ -45,11 +35,6 @@ function Signup({navigation}) {
         <TouchableOpacity onPress={() => navigation.navigate('Login')}><Text style={{color:'#f1681f', textDecorationLine:'underline'}}>Đã có tài khoản?</Text></TouchableOpacity>
         <TouchableOpacity onPress={() => checkSignup()} style={styles.signupBtn}><Text style={{color:'white', fontWeight:'bold'}}>ĐĂNG KÝ</Text></TouchableOpacity>
       </View>
-        
-     
-
-    
-
      </View>
     );
   }
@@ -61,7 +46,7 @@ const styles = StyleSheet.create({
   },
 
   headerContainer: {
-    flex: 0.8,
+    flex: 0.5,
     backgroundColor: '#FFFFFF',
     width: '100%',
     height: '100%',
@@ -85,7 +70,7 @@ const styles = StyleSheet.create({
     fontSize:50,
     color:"#fb5b5a",
     marginBottom:1,
-    marginTop:600,
+    marginTop:580,
     alignSelf:'center', 
  },
 
